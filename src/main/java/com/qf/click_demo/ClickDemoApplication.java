a557958c225b97c3e5fa11a7fcfbf641896155ef
xs <xs@qq.com>
2020-08-27 23:59:01 +0800

    controller添加hello注解

--- src/main/java/com/qf/click_demo/ClickDemoApplication.java
+++ src/main/java/com/qf/click_demo/ClickDemoApplication.java
@@ -4,6 +4,9 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * hello
+ */
 @SpringBootApplication(scanBasePackages = "com.qf")
 @MapperScan("com.qf.dao")
 public class ClickDemoApplication {
